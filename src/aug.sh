fe05a2637eaf863859275b82b7571dfdd53e160c
miraculin1 <2855193562@qq.com>
2025-06-06 21:35:41 +0800

    make aug multi-thread

--- src/aug.sh
+++ src/aug.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 #
-../build/TarTanAirAugmentation /workspace/ematch_ws/src/ematch/DEVO/thirdparty/tartanair_tools/downloaded/datasets/abandonedfactory/Hard/P000/image_left/000100_left.png /workspace/ematch_ws/src/ematch/DEVO/thirdparty/tartanair_tools/downloaded/datasets/abandonedfactory/Hard/P000/depth_left/000100_left_depth.npy
+../build/TarTanAirAugmentation \
+  /workspace/ematch_ws/src/ematch/DEVO/thirdparty/tartanair_tools/downloaded/datasets/abandonedfactory/Hard/P000/image_left/000100_left.png \
+  /workspace/ematch_ws/src/ematch/DEVO/thirdparty/tartanair_tools/downloaded/datasets/abandonedfactory/Hard/P000/depth_left/000100_left_depth.npy
--- src/augmentation.cpp
+++ src/augmentation.cpp
@@ -2,6 +2,7 @@
 #include <filesystem>
 #include <iomanip> // 用于设置输出格式
 #include <iostream>
+#include <omp.h>
 #include <random>
 #include <string>
 #include <utility>
@@ -11,6 +12,7 @@
 #include <cnpy.h>
 #include <opencv2/opencv.hpp>
 
+#include <pcl/common/transforms.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 
@@ -28,54 +30,10 @@ public:
 
   TarTanAirAugmentation() {}
 
-  // 传统逐像素投影
-  Mat reproj_r_t(const Mat &rgb, const Mat &depth, const Matrix3f &R,
-                 const Vector3f &t, bool inpaint = false) {
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr project_pc(const Mat &rgb,
+                                                    const Mat &depth) {
     int height = depth.rows;
     int width = depth.cols;
-    Mat aug_img = Mat::zeros(rgb.size(), rgb.type());
-    Mat depth_buffer = Mat(height, width, CV_32FC1, Scalar(1e6f));
-    for (int v = 0; v < height; v++) {
-      for (int u = 0; u < width; u++) {
-        float Z = depth.at<float>(v, u);
-        if (Z <= 0)
-          continue;
-        float X = (u - cx) * Z / fx;
-        float Y = (v - cy) * Z / fy;
-        Vector3f pt3d(X, Y, Z);
-        Vector3f pt3d_trans = R.transpose() * pt3d + t;
-        float x_proj = (pt3d_trans(0) * fx) / pt3d_trans(2) + cx;
-        float y_proj = (pt3d_trans(1) * fy) / pt3d_trans(2) + cy;
-        if (x_proj < 0 || x_proj >= width || y_proj < 0 || y_proj >= height)
-          continue;
-        int x_round = static_cast<int>(round(x_proj));
-        int y_round = static_cast<int>(round(y_proj));
-        if (x_round < 0 || x_round >= width || y_round < 0 || y_round >= height)
-          continue;
-        float z = pt3d_trans(2);
-        float &buf_z = depth_buffer.at<float>(y_round, x_round);
-        if (z < buf_z) {
-          aug_img.at<Vec3b>(y_round, x_round) = rgb.at<Vec3b>(v, u);
-          buf_z = z;
-        }
-      }
-    }
-    if (inpaint) {
-      Mat gray_mask;
-      cv::inRange(aug_img, Scalar(0, 0, 0), Scalar(0, 0, 0), gray_mask);
-      Mat inpainted;
-      cv::inpaint(aug_img, gray_mask, inpainted, 3, INPAINT_TELEA);
-      return inpainted;
-    }
-    return aug_img;
-  }
-
-  // 使用PCL实现的投影函数
-  Mat reproj_r_t_pcl(const Mat &rgb, const Mat &depth, const Matrix3f &R,
-                     const Vector3f &t, bool inpaint = false) {
-    int height = depth.rows;
-    int width = depth.cols;
-
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(
         new pcl::PointCloud<pcl::PointXYZRGB>());
     cloud->width = width;
@@ -105,30 +63,20 @@ public:
         pt.r = rgb_ptr[u][2];
       }
     }
+    return cloud;
+  }
+
+  Mat reproj_r_t(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud, const Mat &depth,
+                 const Matrix3f &R, const Vector3f &t, bool inpaint = false) {
+    int height = depth.rows;
+    int width = depth.cols;
 
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_transformed(
         new pcl::PointCloud<pcl::PointXYZRGB>());
-    cloud_transformed->width = cloud->width;
-    cloud_transformed->height = cloud->height;
-    cloud_transformed->is_dense = false;
-    cloud_transformed->points.resize(cloud->points.size());
-
-    for (size_t i = 0; i < cloud->points.size(); i++) {
-      const auto &pt_in = cloud->points[i];
-      auto &pt_out = cloud_transformed->points[i];
-      if (!pcl::isFinite(pt_in)) {
-        pt_out = pt_in;
-        continue;
-      }
-      Vector3f p(pt_in.x, pt_in.y, pt_in.z);
-      Vector3f p_trans = R.transpose() * p + t;
-      pt_out.x = p_trans.x();
-      pt_out.y = p_trans.y();
-      pt_out.z = p_trans.z();
-      pt_out.r = pt_in.r;
-      pt_out.g = pt_in.g;
-      pt_out.b = pt_in.b;
-    }
+    Eigen::Affine3f transform = Eigen::Affine3f::Identity();
+    transform.rotate(R);         // 应用旋转矩阵
+    transform.translation() = t; // 应用平移向量
+    pcl::transformPointCloud(*cloud, *cloud_transformed, transform);
 
     Mat aug_img = Mat::zeros(height, width, CV_8UC3);
     Mat depth_buffer(height, width, CV_32FC1, Scalar(1e6f));
@@ -165,24 +113,20 @@ public:
   }
 
   void generate_trajectory_with_gt(const Mat &rgb, const Mat &depth,
-                                   const Matrix3f &R0, const Vector3f &t0,
                                    const vector<Matrix3f> &delta_rotations,
                                    const vector<Vector3f> &delta_translations,
                                    vector<Mat> &images,
                                    vector<pair<Matrix3f, Vector3f>> &gt_poses,
                                    bool inpaint = false, bool use_pcl = false) {
-    Matrix3f R_tar = R0;
-    Vector3f t_tar = t0;
+    auto cloud = project_pc(rgb, depth);
+#pragma omp parallel for
     for (size_t i = 0; i < delta_rotations.size(); i++) {
+      auto R_tar = delta_rotations[i];
+      auto t_tar = delta_translations[i];
       const Matrix3f &dR = delta_rotations[i];
       const Vector3f &dt = delta_translations[i];
-      R_tar = dR * R0;
-      t_tar = dR * t0 + dt;
       Mat img;
-      if (use_pcl)
-        img = reproj_r_t_pcl(rgb, depth, R_tar, t_tar, inpaint);
-      else
-        img = reproj_r_t(rgb, depth, R_tar, t_tar, inpaint);
+      img = reproj_r_t(cloud, depth, R_tar, t_tar, inpaint);
       images.push_back(img);
       gt_poses.emplace_back(R_tar, t_tar);
     }
@@ -224,6 +168,7 @@ public:
 
   void saveImgsAndPoses(
       const std::string &folder_path, const std::vector<cv::Mat> &imgs,
+      const cv::Mat &rgb,
       const std::vector<std::pair<Eigen::Matrix3f, Eigen::Vector3f>>
           &gt_poses) {
     namespace fs = std::filesystem;
@@ -239,10 +184,14 @@ public:
 
     for (size_t i = 0; i < imgs.size(); ++i) {
       // 保存图像
-      std::ostringstream oss;
-      oss << folder_path << "/frame_" << std::setw(3) << std::setfill('0') << i
-          << ".png";
-      cv::imwrite(oss.str(), imgs[i]);
+      std::ostringstream oss0, oss1;
+      oss0 << folder_path << "/frame_" << std::setw(3) << std::setfill('0')
+           << 2 * i << ".png";
+      cv::imwrite(oss0.str(), rgb);
+
+      oss1 << folder_path << "/frame_" << std::setw(3) << std::setfill('0')
+           << 2 * i + 1 << ".png";
+      cv::imwrite(oss1.str(), imgs[i]);
 
       // 转换旋转矩阵到四元数
       const Eigen::Matrix3f &R = gt_poses[i].first;
@@ -250,6 +199,8 @@ public:
       Eigen::Quaternionf q(R);
 
       // 按 tx ty tz qx qy qz qw 格式写一行
+      pose_file << 0 << " " << 0 << " " << 0 << " " << 0 << " " << 0 << " " << 0
+                << " " << 1 << std::endl;
       pose_file << t(0) << " " << t(1) << " " << t(2) << " " << q.x() << " "
                 << q.y() << " " << q.z() << " " << q.w() << std::endl;
     }
@@ -311,15 +262,10 @@ int main(int argc, char **argv) {
 
   TarTanAirAugmentation augmenter;
 
-  // 初始姿态
-  float theta = 2.0f * M_PI / 180.0f;
-  Matrix3f R0 = AngleAxisf(theta, Vector3f(0, 1, 0)).toRotationMatrix();
-  Vector3f t0(0.0f, 0.0f, 0.0f);
-
   // 生成轨迹增量
   vector<Matrix3f> delta_Rs;
   vector<Vector3f> delta_ts;
-  augmenter.generate_delta_rt_6directions(16, 0.1f, 0.01f, 0.01f, delta_Rs,
+  augmenter.generate_delta_rt_6directions(6, 0.1f, 0.01f, 0.01f, delta_Rs,
                                           delta_ts);
   auto t_gen_delta_fin = std::chrono::high_resolution_clock::now();
 
@@ -329,18 +275,18 @@ int main(int argc, char **argv) {
   // 这里可切换 use_pcl = true / false
   bool use_pcl = false;
 
-  augmenter.generate_trajectory_with_gt(rgb, depth, R0, t0, delta_Rs, delta_ts,
-                                        imgs, gt_poses, true, use_pcl);
+  augmenter.generate_trajectory_with_gt(rgb, depth, delta_Rs, delta_ts, imgs,
+                                        gt_poses, true, use_pcl);
   auto t_aug_fin = std::chrono::high_resolution_clock::now();
   auto t_stop = std::chrono::high_resolution_clock::now();
 
-  augmenter.saveImgsAndPoses("./res", imgs, gt_poses);
-  for (size_t i = 0; i < imgs.size(); i++) {
-    imshow("Augmented Image", imgs[i]);
-    int key = waitKey(0);
-    if (key == 27)
-      break; // ESC退出
-  }
+  augmenter.saveImgsAndPoses("./P000/image_left", imgs, rgb, gt_poses);
+  // for (size_t i = 0; i < imgs.size(); i++) {
+  // imshow("Augmented Image", imgs[i]);
+  // int key = waitKey(0);
+  // if (key == 27)
+  // break; // ESC退出
+  // }
 
   printDuration(t_start, t_stop);
   printDuration(t_gen_delta_fin, t_aug_fin);
